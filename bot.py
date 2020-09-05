2bb28ad1927424cb79dd72e08b3face7c6e20cfd
Andreas Rovik <52315788+asc3ndr@users.noreply.github.com>
2020-09-05 22:29:53 +0200

    first pass restructure

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 
-from datetime import datetime
 from dotenv import load_dotenv
 from os import getenv
 import requests
@@ -20,10 +19,8 @@ bot = commands.Bot(command_prefix="!")
 load_dotenv()
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
 CANVAS_TOKEN = getenv("CANVAS_TOKEN")
-
-DB_ANNOUNCEMENTS = Custom.read_JSON("data/announcements.json")
-DB_CONSTANTS = Custom.read_JSON("data/constants.json")
-DB_COURSES = Custom.read_JSON("data/courses.json")
+# NOTE: Before commit, restructure to data/d.json
+DB = Custom.read_JSON("test.json")
 MAKE_CANVAS_API_CALLS = True
 
 
@@ -31,21 +28,21 @@ MAKE_CANVAS_API_CALLS = True
 
 
 async def create_welcome_message():
-    welcome_msg_courses = "".join(
+    courses = "".join(
         [
-            f"\n\t{value['icon']}\t{key}\t{value['name']}"
-            for key, value in DB_COURSES.items()
+            f"\n\t{value['ICON']}\t{key}\t{value['NAME']}"
+            for key, value in DB["COURSES"].items()
         ]
     )
     welcome_msg = f"""
-    For å kunne bruke chat eller voice kanaler på denne serveren må du bli tildelt en Student-rolle av Admin. Vi ber deg vennligst bytte til ditt virkelige navn ved å finne deg selv i 'medlemslisten' til høyre, høyreklikke på deg selv, og klikk på "Bytt Kallenavn / Change Nickname". Det kan ta litt tid før Admin går igjennom medlemslisten, så ta gjerne kontakt når du er klar. :slight_smile:
+    For å kunne bruke chat eller voice kanaler på denne serveren må du bli tildelt en Student-rolle av Admin. Men f;r du roper på Admin så vil vi be deg vennligst bytte til ditt virkelige navn ved å finne deg selv i 'medlemslisten' til høyre, høyreklikke på deg selv, og klikk på "Bytt Kallenavn / Change Nickname".
 
-    For å få tilgang til emne-kanalene; klikk på de korresponderende emojiene under denne meldingen. Du kan fjerne ditt medlemskap fra et emne ved å klikke på den samme emojien om igjen.
+    Deretter kan du få tilgang til dine emne-kanaler ved å klikk på de korresponderende emojiene under denne meldingen. Du kan fjerne ditt medlemskap fra et emne ved å klikke på den samme emojien om igjen. **Merk:** Selv om du har tilgang til en kanal vil du kun ha lese-rettigheter inntil du får innvilget student-rolle fra Admin.
 
-    **Merk:** Selv om du har tilgang til en kanal vil du kun ha lese-rettigheter inntil du får innvilget student-rolle fra Admin.
+    Ta gjerne kontakt med Admin når du er klar, da det kan ta litt tid før vi går igjennom medlemslisten. Takk! :slight_smile:
 
     ```
-    {welcome_msg_courses}
+    {courses}
     ```
     """
 
@@ -55,26 +52,26 @@ async def create_welcome_message():
     welcome_embed.set_author(name="IT og digitalisering")
     welcome_embed.set_thumbnail(url=bot.user.avatar_url)
 
-    welcome_channel = bot.get_channel(DB_CONSTANTS["WELCOME_CHANNEL_ID"])
+    welcome_channel = bot.get_channel(DB["DISCORD"]["WELCOME_CHANNEL_ID"])
     msg = await welcome_channel.send(embed=welcome_embed)
 
-    for value in DB_COURSES.values():
-        await msg.add_reaction(value["icon"])
+    for value in DB["COURSES"].values():
+        await msg.add_reaction(value["ICON"])
 
-    DB_CONSTANTS["WELCOME_CHANNEL_MSG_ID"] = msg.id
-    Custom.write_JSON("constants.json", DB_CONSTANTS)
+    DB["DISCORD"]["WELCOME_CHANNEL_MSG_ID"] = msg.id
+    Custom.write_JSON("test.json", DB)
 
 
 async def create_roles():
-    guild = bot.get_guild(DB_CONSTANTS["GUILD_ID"])
-    for key in DB_COURSES.keys():
+    guild = bot.get_guild(DB["DISCORD"]["GUILD_ID"])
+    for key in DB["COURSES"].keys():
         if not get(guild.roles, name=key):
             await guild.create_role(name=key, permissions=discord.Permissions(67175424))
 
 
 async def canvas_api_fetch_announcement(course_key: str):
 
-    course_id = DB_COURSES[course_key]["id"]
+    course_id = DB["COURSES"][course_key]["CANVAS_ID"]
     if not course_id:
         return
 
@@ -84,23 +81,18 @@ async def canvas_api_fetch_announcement(course_key: str):
 
     data = r.json()
 
-    for post in data:
-        if post["id"] in DB_ANNOUNCEMENTS[course_id]["ID_HISTORY"]:
-            pass
+    for announcement in data:
+        if announcement["id"] in DB["COURSES"][course_key]["ID_HISTORY"]:
+            continue
         else:
-            announcement_url = post["url"]
-            announcement_title = re.sub("<[^<]+?>", "", post["title"])
-            announcement_message = post["message"].replace("</p>", "\n")
+            announcement_title = re.sub("<[^<]+?>", "", announcement["title"])
+            announcement_message = announcement["message"].replace("</p>", "\n")
             announcement_message = re.sub("<[^<]+?>", "", announcement_message)
             announcement_message = announcement_message.strip()
             announcement_message = f"{announcement_message}"
 
             if len(announcement_message) > 1900:
-                announcement_message = (
-                    announcement_message[0:1900]
-                    + "..."
-                    + "\n(meldingen overskrider max antall tegn)"
-                )
+                announcement_message = announcement_message[0:1900] + "..."
 
             announcement_embed = discord.Embed(
                 title=announcement_title,
@@ -108,19 +100,21 @@ async def canvas_api_fetch_announcement(course_key: str):
                 color=0xEDEDED,
             )
             announcement_embed.set_author(
-                name="CANVAS ANNOUNCEMENT", url=announcement_url,
+                name="CANVAS ANNOUNCEMENT", url=announcement["url"],
             )
             announcement_embed.set_thumbnail(url=bot.user.avatar_url)
             announcement_embed.set_footer(
-                text=datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+                text=f"{announcement['posted_at']} by {announcement['user_name']}"
             )
 
-            subject_channel = bot.get_channel(DB_CONSTANTS["BOT_DEV_CHANNEL_ID"])
+            subject_channel = bot.get_channel(DB["DISCORD"]["DEV_CHANNEL_ID"])
             await subject_channel.send(embed=announcement_embed)
 
-            DB_ANNOUNCEMENTS[course_id]["ID_HISTORY"].append(post["id"])
+            DB["COURSES"][course_key]["CANVAS_PAST_ANNOUNCEMENT_IDS"].append(
+                announcement["id"]
+            )
 
-    Custom.write_JSON("data/announcements.json", DB_ANNOUNCEMENTS)
+    Custom.write_JSON("test.json", DB)
 
 
 # EVENTS
@@ -141,13 +135,13 @@ async def on_raw_reaction_add(payload):
     if payload.user_id == bot.user:
         pass
 
-    if payload.message_id != DB_CONSTANTS["WELCOME_CHANNEL_MSG_ID"]:
+    if payload.message_id != DB["DISCORD"]["WELCOME_CHANNEL_MSG_ID"]:
         return
 
-    for key, value in DB_COURSES.items():
-        if payload.emoji.name == value["icon"]:
+    for key, value in DB["COURSES"].items():
+        if payload.emoji.name == value["ICON"]:
             user = payload.member
-            guild = bot.get_guild(DB_CONSTANTS["GUILD_ID"])
+            guild = bot.get_guild(DB["DISCORD"]["GUILD_ID"])
             role = get(guild.roles, name=key)
             try:
                 await discord.Member.add_roles(user, role)
@@ -161,9 +155,9 @@ async def on_raw_reaction_remove(payload):
     if payload.user_id == bot.user:
         pass
 
-    for key, value in DB_COURSES.items():
-        if payload.emoji.name == value["icon"]:
-            guild = bot.get_guild(DB_CONSTANTS["GUILD_ID"])
+    for key, value in DB["COURSES"].items():
+        if payload.emoji.name == value["ICON"]:
+            guild = bot.get_guild(DB["DISCORD"]["GUILD_ID"])
             user = guild.get_member(payload.user_id)
             role = get(guild.roles, name=key)
             try:
